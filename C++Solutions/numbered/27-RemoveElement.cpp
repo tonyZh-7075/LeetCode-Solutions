c6aa45ed274c5741a5094c7dfd8b1a45958a0645
tonyZh-7075 <tonyzh7075@gmail.com>
2026-03-30 22:06:01 -0400

    updated solution to problem 27-1

--- C++Solutions/numbered/27-RemoveElement.cpp
+++ C++Solutions/numbered/27-RemoveElement.cpp
@@ -1,3 +1,21 @@
+/*
+Monday, March 30th, 2026
+
+Topic: Array, Two Pointers
+
+Basic Idea:
+We can use two pointers to solve this problem.
+1. We can use a pointer to iterate through the array and another pointer to keep track of the position where we can place the next element that is not equal to val.
+2. We can iterate through the array and whenever we encounter an element that is not equal to val, we can place it at the position indicated by the second pointer and increment the second pointer.
+3. We can return the value of the second pointer, which will represent the new size of the array.
+
+Time Complexity: O(n), where n is the number of elements in the array.
+
+Notes:
+- The order of the elements can be changed.
+- The elements that are equal to val are not considered in the new array.
+*/
+
 #include <iostream>
 #include <vector>
 
