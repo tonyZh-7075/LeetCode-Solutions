60facef247c5a1437ffeadc5f3129e2892266457
tonyZh-7075 <tonyzh7075@gmail.com>
2026-04-16 18:18:19 -0400

    added solution to problem 412

---
+++ C++Solutions/numbered/412-FizzBuzz.cpp
@@ -0,0 +1,30 @@
+/*
+Thursday, April 16th, 2026
+
+Topic: String, array, Simulation
+
+*/
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> v(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                v[i - 1] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                v[i - 1] = "Fizz";
+            } else if (i % 5 == 0) {
+                v[i - 1] = "Buzz";
+            } else {
+                v[i - 1] = to_string(i);
+            }
+        }
+
+        return v;
+    }
+};
